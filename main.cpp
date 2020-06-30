a7123a961cb541ce31ac52709fcaa3a4e6a86d68
danpeczek <dpeczek93@gmail.com>
2020-06-30 16:05:51 +0200

    conanfile.txt with boost dependency

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
+
+#include "boost/optional.hpp"
+
+boost::optional<std::string> GetString() {
+  return boost::none;
+}
 
 int main(int argc, char *argv[])
 {
   std::cout << "Hello conan" << std::endl;
+  auto str = GetString();
+  if (str) {
+    std::cout << str.get() << std::endl;
+  }
+  else {
+    std::cout << "String is null" << std::endl;
+  }
   return 0;
 }
