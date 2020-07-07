50e401a7ce140969d1525392ca2b2e09fe8f0a7b
danpeczek <dpeczek93@gmail.com>
2020-07-07 09:50:30 +0200

    Still tests are not working

--- conanfile.py
+++ conanfile.py
@@ -2,13 +2,21 @@ from conans import ConanFile, CMake
 
 class BoostConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "boost/1.73.0", "gtest/1.10.0" # comma-separated list of requirements
+   
    generators = "cmake"
+   
    default_options = {"boost:shared": True}
+
+   requires = ("boost/1.73.0") # comma-separated list of requirements
+   
+   build_requires = (
+      "gtest/[>=1.8.0]"
+   )
 
    def imports(self):
       self.copy("*.dll", dst="bin", src="bin") # From bin to bin
       self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+      self.copy("*.so*", dst="bin", src="lib") # From lib to bin
    
    def build(self):
       cmake = CMake(self)
